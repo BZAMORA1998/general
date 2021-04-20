50b60a70e5fcb6180115f9cbb1ca68939619ec9e
Bryan <bryan.zamora@goitsa.me>
2021-04-20 09:37:46 -0500

    Initial commit

--- com.general.configuracion/Configuracion.js
+++ com.general.configuracion/Configuracion.js
@@ -26,7 +26,7 @@ class Configuracion {
         app.use(cors())
 
         //Declara en que puerto de quiere trabajar
-        app.set('port',process.env.PORT||5000)
+        app.set('port',process.env.PORT||8080)
 
         return app;
 
